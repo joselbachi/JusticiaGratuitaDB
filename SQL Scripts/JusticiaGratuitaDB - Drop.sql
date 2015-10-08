70b6b2c4bdac097ca1100ea1673755ac010c9f65
joseluis.bachiller <joseluis.bachiller@W420SORIA006.DSIC.ES>
2015-10-08 15:05:09 +0200

    Añadido proyecto al NetBeans

---
+++ SQL Scripts/JusticiaGratuitaDB - Drop.sql
@@ -0,0 +1,24 @@
+drop table PERSONA;
+drop table USUARIO;
+drop table JUZGADO;
+drop table ABOGADO;
+drop table PROCURADOR;
+drop table COMISION;
+drop table EXPEDIENTE;
+drop table TJUZGADO;
+drop table TASUNTO;
+drop table TEXPEDIENTE;
+drop table ESTADOEXP;
+drop table TPERFIL;
+drop table TRANSESTADOEXP;
+
+DROP SEQUENCE PUBLIC.SEQ_COMISION_ID;
+DROP SEQUENCE SEQ_Persona_id;
+DROP SEQUENCE SEQ_Juzgado_id;
+DROP SEQUENCE SEQ_Comision_id;
+DROP SEQUENCE SEQ_Expediente_id;
+DROP SEQUENCE SEQ_Expediente_Turno;
+DROP SEQUENCE SEQ_TJUZGADO_id;
+DROP SEQUENCE SEQ_TASUNTO_id;
+DROP SEQUENCE SEQ_TEXPED_id;
+DROP SEQUENCE SEQ_TPERFIL_id;
